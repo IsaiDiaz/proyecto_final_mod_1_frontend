b49e4712a52d7e14f9dcae49f51e31068ce7aff7
IsaiDiaz <jsoza057@gmail.com>
2025-05-13 10:05:53 -0400

    fix: corregida peticion a refresh token

--- src/services/apiPrivate.js
+++ src/services/apiPrivate.js
@@ -23,7 +23,7 @@ apiPrivate.interceptors.response.use(
     const originalRequest = error.config;
 
     if (
-      error.response?.status === 401 &&
+      error.response?.status === 403 || error.response?.status === 401 &&
       !originalRequest._retry
     ) {
       originalRequest._retry = true;
